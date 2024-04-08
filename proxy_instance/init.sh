6b5a6cb329c61615c0c347fe019f8c8e588d7e53
unknown <leonardo_vieiracosta@hotmail.com>
2024-04-08 20:48:42 -0300

    Configurações iniciais

---
+++ proxy_instance/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# git clone https://github.com/ShobiExplains/fastapi-deployment-demo.git fastapi
+
+cd fastapi
+
+mkdir --parents /var/www/fastapi
+mv src /var/www/fastapi
+mv requirements.txt /var/www/fastapi
+
+cd /var/www/fastapi
+
+python3 -m venv env
+source env/bin/activate
+pip3 install -r requirements.txt
+
+chown  www-data:www-data -R /var/www
