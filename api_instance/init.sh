a569e6c22f7380109865d1ab92e4160004477c69
unknown <leonardo_vieiracosta@hotmail.com>
2024-04-09 09:10:36 -0300

    Ajuste no arquivo de inicialização

--- api_instance/init.sh
+++ api_instance/init.sh
@@ -3,26 +3,33 @@
 # Define o diretório do script
 directory=$(dirname "$0")
 
+cd "$directory"
+
+echo $pwd
+
 # Atualiza e instala pacotes necessários
 apt-get update -y
 apt-get install python3-venv python3-pip supervisor -y
 
-# Verifica a instalação do Supervisor
-if ! which supervisorctl > /dev/null 2>&1; then
-    echo "Erro: Supervisor não foi instalado corretamente."
-    exit 1
-fi
+# # Verifica a instalação do Supervisor
+# if ! which supervisorctl > /dev/null 2>&1; then
+#     echo "Erro: Supervisor não foi instalado corretamente."
+#     exit 1
+# fi
 
-# Verifica o status do serviço Supervisor
-if ! systemctl status supervisor > /dev/null 2>&1; then
-    echo "Erro: O serviço Supervisor não está em execução."
-    exit 1
-fi
+# # Verifica o status do serviço Supervisor
+# if ! systemctl status supervisor > /dev/null 2>&1; then
+#     echo "Erro: O serviço Supervisor não está em execução."
+#     exit 1
+# fi
 
 # Cria e move arquivos para o diretório de aplicação
-cd ~/fastapi/api_instance
+# cd "$directory/fastapi/api_instance"
 mkdir -p /var/www/fastapi
-mv api /var/www/fastapi
+mv api/* /var/www/fastapi
+
+# Move arquivos do Supervisor
+mv etc/supervisor/conf.d/fastapi-gunicorn.conf /etc/supervisor/conf.d/
 
 # Navega até o diretório de aplicação e configura o ambiente virtual
 cd /var/www/fastapi
@@ -33,15 +40,15 @@ pip3 install -r requirements.txt
 # Define permissões de usuário para o diretório
 chown www-data:www-data -R /var/www
 
-# Configura o Supervisor
-mv ~/fastapi/api_instance/etc/supervisor/conf.d/fastapi-gunicorn.conf /etc/supervisor/conf.d/
+# Habilita o supervisor
 supervisorctl reread
 supervisorctl update
 
-# Verifica se a configuração do Supervisor está correta
-if ! supervisord -c /etc/supervisor/supervisord.conf -n; then
-    echo "Erro: A configuração do Supervisor não é válida."
-    exit 1
-fi
+
+# # Verifica se a configuração do Supervisor está correta
+# if ! supervisord -c /etc/supervisor/supervisord.conf -n; then
+#     echo "Erro: A configuração do Supervisor não é válida."
+#     exit 1
+# fi
 
 echo "Instalação e configuração do Supervisor concluídas com sucesso."
